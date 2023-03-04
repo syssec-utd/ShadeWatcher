2afc6c063bdc20675b6312c16710e26bb49ca457
Nick Baker <ndbaker1@outlook.com>
2023-03-04 00:34:49 -0600

    add extra summary phase to make it easier to see results and what still needs fixing

--- syssec-data-processing/quick-run-datasets.sh
+++ syssec-data-processing/quick-run-datasets.sh
@@ -4,9 +4,9 @@
 
 shopt -s globstar
 
-OUTPUT_DIR=${OUTPUT_DIR:-dataset-logs}
+OUTPUT_DIR=${OUTPUT_DIR:-"dataset-logs"}
 
-mkdir -p ./$OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
 
 # expected to be passed paths to multiple dataset folders
 # use globstar matching or preferred method to fill these paths.
@@ -14,7 +14,17 @@ mkdir -p ./$OUTPUT_DIR
 #   /datasets/APT_CASE_1*/stage{1,2,3,4,5}/*
 for f in "$@"
 do
-        EPOCH=${EPOCH:-100} ./process-dataset.sh $f &> "./$OUTPUT_DIR/$(sed 's/\/\+/-/g' <<< $f | sed 's/^-\+//').log" &
+    EPOCH=${EPOCH:-100} ./process-dataset.sh $f &> "$OUTPUT_DIR/$(sed 's/\/\+/-/g' <<< $f | sed 's/^-\+//').log" &
 done
+
 # wait for all processing jobs to finish
 wait
+
+# print short info about what we want.
+rm -f $OUTPUT_DIR/summary.log
+for f in $OUTPUT_DIR/*
+do
+    echo -e ">>> $f\n\n$(tail -n 2 $f)\n" | tee -a $OUTPUT_DIR/summary.log
+done
+
+echo "results posted to $OUTPUT_DIR/summary.log"
