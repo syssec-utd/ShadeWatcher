6818d1bfddb7c07c1b0f92f55a3a540f554318f8
Nick Baker <ndbaker1@outlook.com>
2023-03-21 19:45:29 +0000

    change format for percentage improvement script

--- syssec-data-processing/parse-dataset-csv-percent-change.py
+++ syssec-data-processing/parse-dataset-csv-percent-change.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
 
     from collections import defaultdict
 
-    graphs = defaultdict(dict)
+    # case :: stage :: {GADGET, NON-GADGET}
+    case_comparisons = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 
     for csv_path in glob.glob(f"{csv_dir}/*.csv"):
         df = pandas.read_csv(csv_path)
@@ -29,80 +30,64 @@ if __name__ == "__main__":
         df = df.dropna()
         df = df.sort_values(by="detection_ratio")
 
-        # apply +1 smoothing, beacause we are going to do more processing
-
-        avg_tn, avg_fp = (
-            df["true_negative"].sum() / len(df.index) + 1,
-            df["false_positive"].sum() / len(df.index) + 1,
-        )
         max_tn, max_fp = (
-            df.iloc[-1]["true_negative"].astype(float) + 1,
-            df.iloc[-1]["false_positive"].astype(float) + 1,
+            df.iloc[-1]["true_negative"].astype(float),
+            df.iloc[-1]["false_positive"].astype(float),
         )
+
         min_tn, min_fp = (
-            df.iloc[0]["true_negative"].astype(float) + 1,
-            df.iloc[0]["false_positive"].astype(float) + 1,
+            df.iloc[0]["true_negative"].astype(float),
+            df.iloc[0]["false_positive"].astype(float),
         )
 
         case, stage, program, *_ = csv_path[csv_path.index("APT") :].split("-")
 
-        graphs[f"{stage}-{program}"][case] = (
-            case,
-            stage,
-            program,
-            avg_tn,
-            avg_fp,
-            max_tn,
-            max_fp,
-            min_tn,
-            min_fp,
+        stage_number = "stage3-5" if int(stage[-1]) in [3, 4, 5] else stage
+
+        case_number = int("".join(c for c in case if c.isdigit()))
+        case_enum = "Enterprise APT" if case_number == 1 else "Supply-Chain APT"
+
+        if "GADGET" in case:
+            case_type = "GADGET"
+        else:
+            case_type = "NON-GADGET"
+
+        # apply +1 smoothing, beacause we are going to do more processing
+        case_comparisons[case_enum][stage_number][case_type].append(
+            (
+                max_fp + 1,
+                max_tn + 1,
+                min_fp + 1,
+                min_tn + 1,
+            )
         )
 
-    for _, cases in graphs.items():
-        if len(cases) == 2:
-            case1, case2 = cases.keys()
-            if "GADGET" in case1:
-                general, base = cases.values()
-            else:
-                base, general = cases.values()
+    table = list()
 
-            (
-                case,
-                stage,
-                program,
-                avg_tn,
-                avg_fp,
-                max_tn,
-                max_fp,
-                min_tn,
-                min_fp,
-            ) = base
-
-            (
-                _,
-                _,
-                _,
-                gadget_avg_tn,
-                gadget_avg_fp,
-                gadget_max_tn,
-                gadget_max_fp,
-                gadget_min_tn,
-                gadget_min_fp,
-            ) = general
-
-            # gadget/base
-            avg_imp = (gadget_avg_tn * avg_fp) / (gadget_avg_fp * avg_tn) - 1
-            max_imp = (gadget_max_tn * max_fp) / (gadget_max_fp * max_tn) - 1
-            min_imp = (gadget_min_tn * min_fp) / (gadget_min_fp * min_tn) - 1
-
-            TERMS = "|".join(
-                [
-                    stage,
-                    program,
-                    f"{avg_imp * 100:.0f}%",
-                    f"{max_imp * 100:.0f}%",
-                    f"{min_imp * 100:.0f}%",
-                ]
+    for case_number, cases in case_comparisons.items():
+        for stage_number, stage in cases.items():
+            # find the best improvement among cases in the stage
+            best_non_gadget = max(
+                stage["NON-GADGET"],
+                key=lambda e: e[3] / e[2],
+            )
+            best_gadget = max(
+                stage["GADGET"],
+                key=lambda e: e[1] / e[0],
             )
 
-            print(f"|{TERMS}|")
+            percent_improvement_tn = ((best_gadget[1] / best_non_gadget[1]) - 1) * 100
+            percent_improvement_fp = ((best_gadget[0] / best_non_gadget[0]) - 1) * 100
+
+            table.append(
+                (
+                    case_number,
+                    stage_number,
+                    f"[{percent_improvement_fp:.0f}%, {percent_improvement_tn:.0f}%]",
+                )
+            )
+
+    table_df = pandas.DataFrame(table, columns=["APT", "stage", "change"])
+    table_df = table_df.pivot(index="APT", columns="stage", values="change")
+
+    print(table_df.to_markdown())
