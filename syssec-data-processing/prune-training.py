bc9b719f4c09c1ddd5edd5dd0cf74a43fad71295
Nick Baker <ndbaker1@outlook.com>
2023-04-09 23:06:34 -0500

    cleaning interfaces

--- syssec-data-processing/prune-training.py
+++ syssec-data-processing/prune-training.py
@@ -4,22 +4,12 @@ building a frequency database of the edges and removing entries
 below a given frequency threshold
 """
 
-if __name__ == "__main__":
-    import argparse
-    import glob
-    import sys
-    from collections import defaultdict
+import argparse
+import glob
+import sys
+from collections import defaultdict
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--threshold", default=2, type=int, help="the frequency threshold for training entries before they are dropped")
-    parser.add_argument("encoding_path", help="path to the directory of the training encodings (usually <SHADEWATCHER_DIR>/data/encoding/...)")
-    args = parser.parse_args()
-
-    print(args, file=sys.stderr)
-
-    encoding_path = args.encoding_path
-    threshold = args.threshold
-
+def prune(encoding_path, threshold):
     frequency_db = defaultdict(int)
 
     with open(f"{encoding_path}/train2id.txt") as train_file:
@@ -39,3 +29,17 @@ if __name__ == "__main__":
             if freq >= threshold:
                 for _ in range(freq):
                     train_file.write(key + "\n")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--threshold", default=2, type=int, help="the frequency threshold for training entries before they are dropped")
+    parser.add_argument("encoding_path", help="path to the directory of the training encodings (usually <SHADEWATCHER_DIR>/data/encoding/...)")
+    args = parser.parse_args()
+
+    print(args, file=sys.stderr)
+
+    encoding_path = args.encoding_path
+    threshold = args.threshold
+
+    prune(encoding_path, threshold)
---
+++ syssec-data-processing/shadewatcher-runner.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+    # Parsing Step (Parallel / Cached)
+    # 1. parse training data and aggregate into single directory
+    # 2. train a model on the dataset
+    # 3. parse testing datasets and store individually
+
+    # Evaluation Step
+    # 1. pad the test instance to be the size of the model
+    # 2. run the test instance against the model
+    # 3. store the testing summary in a file (based on a session key)
+    pass
