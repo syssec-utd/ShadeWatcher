a877f0a7b76cccba5a8037abc4f53c72875ffca5
Nick Baker <ndbaker1@outlook.com>
2023-05-02 18:14:06 +0000

    edit analyzing script

--- syssec-data-processing/compare_eval.py
+++ syssec-data-processing/compare_eval.py
@@ -12,6 +12,10 @@ def f1_score(tp, fp, fn):
     return 2 * prec * rec / (prec + rec)
 
 
+def fpr(fp, tn):
+    return fp / (fp + tn)
+
+
 if __name__ == "__main__":
     import argparse
     import pandas
@@ -24,7 +28,7 @@ if __name__ == "__main__":
         "--smoothing",
         help="constant for laplace smoothing the tn/fp result dataset",
         type=int,
-        default=1,
+        default=0,
     )
     args = parser.parse_args()
 
@@ -33,30 +37,46 @@ if __name__ == "__main__":
     csv_dir = args.csv_dir
     smoothing = args.smoothing
 
-    TRUE_NEGATIVE_KEY = "true_negative"
-    FALSE_POSITIVE_KEY = "false_positive"
+    TRUE_NEGATIVE_KEY = "tn"
+    FALSE_POSITIVE_KEY = "fp"
+
+    TRUE_POSITIVE_KEY = "tp"
+    FALSE_NEGATIVE_KEY = "fn"
 
     agg_df = pandas.DataFrame()
 
     for csv_path in glob.glob(f"{csv_dir}/*.csv"):
         df = pandas.read_csv(csv_path).dropna()
         df = df.assign(filename=csv_path)
-        df[FALSE_POSITIVE_KEY] = df[FALSE_POSITIVE_KEY].apply(lambda v: v + smoothing)
-        df[TRUE_NEGATIVE_KEY] = df[TRUE_NEGATIVE_KEY].apply(lambda v: v + smoothing)
-        df = df.assign(
-            f1=lambda x: f1_score(
-                tp=x[FALSE_POSITIVE_KEY],
-                fp=smoothing,
-                fn=x[TRUE_NEGATIVE_KEY],
-            )
-        )
-        df = df.assign(
-            recall=lambda x: recall(
-                tp=x[FALSE_POSITIVE_KEY],
-                fn=x[TRUE_NEGATIVE_KEY],
-            )
-        )
-
         agg_df = pandas.concat([agg_df, df])
 
-    print(agg_df.groupby("filename").mean().round(2).to_markdown())
+    # sum scores based on instance
+    agg_df = agg_df.groupby("filename").sum()
+    agg_df[FALSE_POSITIVE_KEY] = agg_df[FALSE_POSITIVE_KEY].apply(
+        lambda v: v + smoothing,
+    )
+    agg_df[TRUE_NEGATIVE_KEY] = agg_df[TRUE_NEGATIVE_KEY].apply(
+        lambda v: v + smoothing,
+    )
+
+    agg_df = agg_df.assign(
+        f1=lambda x: f1_score(
+            tp=x[TRUE_POSITIVE_KEY],
+            fp=x[FALSE_POSITIVE_KEY],
+            fn=x[FALSE_NEGATIVE_KEY],
+        )
+    )
+    agg_df = agg_df.assign(
+        recall=lambda x: recall(
+            tp=x[TRUE_POSITIVE_KEY],
+            fn=x[FALSE_NEGATIVE_KEY],
+        )
+    )
+    agg_df = agg_df.assign(
+        fpr=lambda x: fpr(
+            fp=x[FALSE_POSITIVE_KEY],
+            tn=x[TRUE_NEGATIVE_KEY],
+        )
+    )
+
+    print(agg_df.round(4).to_markdown())
