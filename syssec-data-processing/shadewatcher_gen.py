89bfe69aef9febb261dd47e6543f0e5ecf648427
Nick Baker <ndbaker1@outlook.com>
2023-05-03 11:16:02 -0500

    update values and add gan

--- syssec-data-processing/shadewatcher_gen.py
+++ syssec-data-processing/shadewatcher_gen.py
@@ -1,15 +1,6 @@
 # generate commands for shadewatcher
 import os
 
-datasets = [
-    "tc3-trace",
-    "tc3-theia",
-    "tc3-fiveD",
-    "tc5-trace",
-    "tc5-theia",
-    "tc5-fiveD2",
-]
-
 parse_paths = " ".join(
     [
         "/syssec_nas0/prov_graphs/darpa/APT/TC3/*/*/nd/*.json",
@@ -19,29 +10,31 @@ parse_paths = " ".join(
 )
 
 gnn_args = {
-    "tc3-trace": "--epoch 43 --threshold 1.5",
-    "tc3-theia": "--epoch 85 --threshold 1.5",
-    "tc3-fiveD": "--epoch 30 --threshold 1.5",
-    "tc5-trace": "--epoch 85 --threshold 1.5",
-    "tc5-theia": "--epoch 80 --threshold 1.5",
-    "tc5-fiveD2": "--epoch 30 --threshold 1.5",
+    "tc3-trace": "--epoch 10",
+    "tc3-theia": "--epoch 10",
+    "tc3-fiveD": "--epoch 30",
+    "tc5-trace": "--epoch 15",
+    "tc5-theia": "--epoch 17",
+    "tc5-fiveD2": "--epoch 30",
+    "gan": "--epoch 30",
 }
 
 benign_paths = {
     "tc3-trace": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc3-trace-*",
     "tc3-theia": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc3-theia-*",
-    "tc3-fiveD": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc3-fiveD-*",
+    # "tc3-fiveD": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc3-fiveD-*",
     "tc5-trace": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc5-trace-*",
     "tc5-theia": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc5-theia-*",
-    "tc5-fiveD2": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc5-fiveD2-*",
+    # "tc5-fiveD2": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-benign-tc5-fiveD2-*",
+    "gan": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-reconstructed-*",
 }
 anomaly_paths = {
     "tc3-trace": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC3-trace-*",
     "tc3-theia": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC3-theia-*",
-    "tc3-fiveD": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC3-fivedirections-*",
+    # "tc3-fiveD": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC3-fivedirections-*",
     "tc5-trace": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC5-TRACE-*",
     "tc5-theia": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC5-THEIA-*",
-    "tc5-fiveD2": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC5-FiveDirections-*",
+    # "tc5-fiveD2": "shadewatcher_store/syssec_nas0-prov_graphs-darpa-APT-TC5-FiveDirections-*",
 }
 
 python = "python3.6"
@@ -55,22 +48,23 @@ test_output_dir = "darpa-tests"
 
 print(f"mkdir -p {test_output_dir}")
 
-for dset in datasets:
-    assert dset in benign_paths and dset in anomaly_paths and dset in gnn_args
 assert os.path.exists(eval_script)
 assert os.path.exists(parse_script)
 assert os.path.exists(train_script)
 
 print(f"{python} {parse_script} '{parse_paths}'")
 
-for train in datasets:
+for train in benign_paths.keys():
     arg_string = gnn_args[train].strip().replace("-", "").replace(" ", "_")
     model_name = f"{train}_{arg_string}"
     print(
         f"{python} {train_script} '{benign_paths[train]}' {model_name} --gnn_args='{gnn_args[train]}'"
     )
 
-    for test in datasets:
+    for test in set(anomaly_paths.keys()).intersection(set(benign_paths.keys())):
+        if test not in anomaly_paths or test not in benign_paths:
+            continue
+
         test_name = f"{train}_{test}.csv"
         print(
             f"{python} {eval_script} '{anomaly_paths[test]}' shadewatcher_store/{model_name} {test_output_dir}/{test_name}"
